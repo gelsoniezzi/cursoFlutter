746b8748f05fcb532fb8cbfaff48dfaa7e88b94c
gelsoniezzi <gelsoniezzi@MacBook-Pro-de-Gelson.local>
2025-05-02 16:33:26 -0300

    Melhorias no card despesa (transaction)

--- expenses_alone/lib/main.dart
+++ expenses_alone/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses_alone/models/expense.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 
 void main () =>  runApp(const ExpensesApp());
@@ -27,6 +28,7 @@ class Home extends StatelessWidget {
     Expense(id: "e4", title: "Camisa", value: 60.00, date: DateTime.now()),
     Expense(id: "e5", title: "Perfume", value: 78.00, date: DateTime.now()),    
   ];
+  
 
   @override
   Widget build(BuildContext context){
@@ -52,15 +54,17 @@ class Home extends StatelessWidget {
                             Column(
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Text("${d.title}"),
-                                Text("${d.date}", ),
+                                Text(
+                                  "${d.title}", style: const TextStyle(fontWeight: FontWeight.bold),),
+                                Text(
+                                  DateFormat('dd/MM/yyyy hh:mm').format(d.date), style: TextStyle(color: Colors.black45)),
                               ],
                             ),
                             Container(
                               padding: EdgeInsets.all(8),
                               
                               decoration: BoxDecoration(color: const Color.fromARGB(255, 188, 188, 188), borderRadius: BorderRadius.all(Radius.circular(8))),
-                              child: Text("${d.value}"),
+                              child: Text("R\$ ${d.value.toStringAsFixed(2)}"),
                               
 
                             ),
--- expenses_alone/lib/models/expense.dart
+++ expenses_alone/lib/models/expense.dart
@@ -2,13 +2,13 @@ class Expense {
   final String id;
   final String title;
   final double value;
-  final DateTime? date;
+  final DateTime date;
 
   // construtor
   Expense({
     required this.id,
     required this.title,
     required this.value,
-    this.date,
+    required this.date,
   });
 }
