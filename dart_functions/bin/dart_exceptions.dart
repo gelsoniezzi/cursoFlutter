5fec3725387c5308a0127e0a5d87cab7bcb6e796
gelsoniezzi <gelsoniezzi@MacBook-Pro-de-Gelson.local>
2025-05-21 15:09:52 -0300

    Excecoes, erros assert

--- dart_functions/bin/dart_exceptions.dart
+++ dart_functions/bin/dart_exceptions.dart
@@ -30,6 +30,20 @@ String resultado(double n1, double n2, double n3){
 
 }
 
+class Motorista {
+    final String nome;
+    final int idade;
+
+    Motorista(this.nome, this.idade){
+      if (nome.isEmpty){
+        throw AssertionError('O nome não pode ser vazio.');
+      } else if(idade < 18){
+        throw AssertionError('A o motorista deve ser maior de idade.');
+      }
+    }
+
+  }
+
 void main(){
 
   List<double> notasAlunoR = [10.0, 7.5, 3]; // notas recuperação
@@ -56,5 +70,31 @@ void main(){
     print(e); // Parâmetro repassado pela exceção
   }
 
+  //----------------------------------------------------------
+  // Exceção nomeada
+
+  try{
+    print(resultado(notasAlunoA[0],notasAlunoA[1],notasAlunoA[2]));
+  } on NotaForadoPadrao {
+    print("Erro nota fora do padrão.");
+  }catch (e) {
+    print(e);
+  } finally{
+    print('O finally sempre é executado após um try.');
+  }
+  //----------------------------------------------------------
+  
+  
+  // teste de assert
+  int? id = 2;
+
+  // A condição que deve ser satisfeita, caso contrário a exceção é disparada e o código para em desenvolvimento.
+  assert(id != null, "o id não pode ser nulo.");
+  print('$id');
+
+  // 
+  Motorista m1 = new Motorista('', 17); // Observe o erro: Unhandled exception: Assertion failed: "O nome não pode ser vazio."
+  
+  Motorista m2 = new Motorista('Wilson', 17); // Observe o erro
 }
 
